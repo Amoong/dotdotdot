275cf73bdda0e87a25449994d930787bbfbe0e0b
Amoong <moo1323@naver.com>
2023-10-24 20:38:09 +0900

    Set baseColor

--- components/ColorMenu/Pallet/ColorBar.tsx
+++ components/ColorMenu/Pallet/ColorBar.tsx
@@ -1,11 +1,28 @@
 "use client";
 
+import { useColorStore } from "@/store/color";
 import RangeBar from "./RangeBar";
-import { BAR_CANVAS_HEIGHT } from "./constants";
+import { BAR_CANVAS_HEIGHT, BAR_CANVAS_WIDTH } from "./constants";
 
 function ColorBar() {
+  const setBaseColor = useColorStore((state) => state.setBaseColor);
+
   const drawPreviewColors = (ctx: CanvasRenderingContext2D) => {
-    const linearGradient = ctx.createLinearGradient(0, 0, 0, BAR_CANVAS_HEIGHT);
+    // 처음부터 끝까지 그라데이션으로 채우면 위 - 아래 끝 픽셀을 추출해도
+    // 정확히 #ff0000 값이 안 나오고 근사치만 나와서
+    // 위 - 아래 끝 부분이 확실하게 #ff0000으로 되도록 함
+
+    const linearGradient = ctx.createLinearGradient(
+      0,
+      0,
+      0,
+      BAR_CANVAS_HEIGHT - 2,
+    );
+
+    ctx.fillStyle = "#ff0000";
+
+    ctx.fillRect(0, 0, BAR_CANVAS_WIDTH, BAR_CANVAS_HEIGHT);
+
     linearGradient.addColorStop(0, "#ff0000");
     linearGradient.addColorStop(1 / 6, "#ffff00");
     linearGradient.addColorStop(2 / 6, "#00ff00");
@@ -16,10 +33,30 @@ function ColorBar() {
 
     ctx.fillStyle = linearGradient;
 
-    ctx.fillRect(0, 0, BAR_CANVAS_HEIGHT, BAR_CANVAS_HEIGHT);
+    ctx.fillRect(0, 1, BAR_CANVAS_WIDTH, BAR_CANVAS_HEIGHT - 1);
   };
 
-  return <RangeBar drawPreviewColors={drawPreviewColors} />;
+  const onSliderMove = (info: {
+    value: number;
+    ctx: CanvasRenderingContext2D;
+  }) => {
+    const yPos = Math.min(
+      info.value * BAR_CANVAS_HEIGHT,
+      BAR_CANVAS_HEIGHT - 1,
+    );
+
+    const pixel = info.ctx.getImageData(2, yPos, 1, 1);
+    const data = pixel.data;
+
+    setBaseColor({ r: data[0], g: data[1], b: data[2] });
+  };
+
+  return (
+    <RangeBar
+      drawPreviewColors={drawPreviewColors}
+      onSliderMove={onSliderMove}
+    />
+  );
 }
 
 export default ColorBar;
--- store/color.tsx
+++ store/color.tsx
@@ -1,10 +1,12 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
+type BaseColor = { r: number; g: number; b: number };
 interface ColorState {
-  baseColor: { r: number; g: number; b: number };
+  baseColor: BaseColor;
   opacity: number;
   color: string;
+  setBaseColor: (baseColor: BaseColor) => void;
   setOpacity: (opacity: number) => void;
   setColor: (color: string) => void;
 }
@@ -15,6 +17,7 @@ export const useColorStore = create<ColorState, [["zustand/devtools", never]]>(
       baseColor: { r: 255, g: 0, b: 0 },
       opacity: 1,
       color: "#ff0000",
+      setBaseColor: (baseColor: BaseColor) => set(() => ({ baseColor })),
       setOpacity: (opacity: number) =>
         set(() => ({
           opacity,
