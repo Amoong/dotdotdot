f47f03f743536c44854a97a702cdb41859deacc4
Amoong <moo1323@naver.com>
2023-10-25 20:55:33 +0900

    Update ColorBar drawing logic

--- components/ColorMenu/Pallet/ColorBar.tsx
+++ components/ColorMenu/Pallet/ColorBar.tsx
@@ -8,32 +8,21 @@ function ColorBar() {
   const setBaseRgb = useColorStore((state) => state.setBaseRgb);
 
   const drawPreviewColors = (ctx: CanvasRenderingContext2D) => {
-    // 처음부터 끝까지 그라데이션으로 채우면 위 - 아래 끝 픽셀을 추출해도
-    // 정확히 #ff0000 값이 안 나오고 근사치만 나와서
-    // 위 - 아래 끝 부분이 확실하게 #ff0000으로 되도록 함
+    const linearGradient = ctx.createLinearGradient(0, 0, 0, BAR_CANVAS_HEIGHT);
 
-    const linearGradient = ctx.createLinearGradient(
-      0,
-      0,
-      0,
-      BAR_CANVAS_HEIGHT - 2,
-    );
-
-    ctx.fillStyle = "#ff0000";
-
-    ctx.fillRect(0, 0, BAR_CANVAS_WIDTH, BAR_CANVAS_HEIGHT);
-
-    linearGradient.addColorStop(0, "#ff0000");
+    // edge color of gradient is not exact #ff0000
+    // so adjust edge point a little
+    linearGradient.addColorStop(0.02, "#ff0000");
     linearGradient.addColorStop(1 / 6, "#ffff00");
     linearGradient.addColorStop(2 / 6, "#00ff00");
     linearGradient.addColorStop(3 / 6, "#00ffff");
     linearGradient.addColorStop(4 / 6, "#0000ff");
     linearGradient.addColorStop(5 / 6, "#ff00ff");
-    linearGradient.addColorStop(1, "#ff0000");
+    linearGradient.addColorStop(0.98, "#ff0000");
 
     ctx.fillStyle = linearGradient;
 
-    ctx.fillRect(0, 1, BAR_CANVAS_WIDTH, BAR_CANVAS_HEIGHT - 1);
+    ctx.fillRect(0, 0, BAR_CANVAS_WIDTH, BAR_CANVAS_HEIGHT);
   };
 
   const onSliderMove = (info: {
